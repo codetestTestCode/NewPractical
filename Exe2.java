efce1bd6262cc224c9fb30493eb30ef9a7f4cf5a
codetestTestCode <mmg36003@gmail.com>
2020-10-17 18:29:57 +0630

    first practical

---
+++ Exe2.java
@@ -0,0 +1,17 @@
+public class Exe2 {
+    public static void main(String[] args) {
+        // for odd Number   (Stat =1 ; add= 2)
+       for(int x =1;x<=100;x+=2){
+           System.out.print(x+":");
+       }
+        System.out.println();
+       // for even Number   (Stat =0 ; add =2)
+       int i = 0 ;
+        while (i<=100){
+            System.out.print(i+":");
+            i+= 2;
+        }
+    }
+}
+
+
